5998d80c764dada6def5f4fe5f3e87d62ab33976
Kelsey Bowen <kelseybowen22@gmail.com>
2023-09-26 16:25:10 -0700

    algos practice

--- main.py
+++ main.py
@@ -65,3 +65,79 @@ def intersect(nums1, nums2):
             result.append(num)
             count[num] -= 1
     return result
+
+# PLUS ONE
+def plusOne(digits):
+    if digits[-1] != 9:
+        digits[-1] += 1
+    else:
+        str_dig = ""
+        for dig in digits:
+            str_dig += str(dig)
+        res = int(str_dig) + 1
+        digits = []
+        for dig in str(res):
+            digits.append(int(dig))
+    return digits
+
+# MOVE ZEROES
+def moveZeroes(nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    i = 0
+    count = 0
+    while i < len(nums):
+        if nums[i] == 0:
+            count += 1
+            nums.pop(i)
+        else:
+            i += 1
+    for i in range(count):
+        nums.append(0)
+    # slower solution
+    # l, r = 0, 1
+    # while r < len(nums):
+    #     if nums[l] == 0:
+    #         if nums[r] == 0:
+    #             r += 1
+    #             continue
+    #         else:
+    #             nums[l], nums[r] = nums[r], nums[l]
+    #             l += 1
+    #     else:
+    #         l += 1
+    #     r += 1
+
+# TWO SUM
+def twoSum(nums, target):
+    seen = {}
+    for i in range(len(nums)):
+        comp = target - nums[i]
+        if comp in seen:
+            return [seen[comp], i]
+        else:
+            seen[nums[i]] = i
+
+# MAX DEPTH OF BINARY TREE
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# class Solution:
+    maxDep = 1
+    def maxDepth(self, root):
+        if not root:   
+            return 0
+        
+        def dft(root, depth):
+            if root.left:
+                dft(root.left, depth+1)
+            if root.right:
+                dft(root.right, depth+1)
+            if depth > self.maxDep:
+                self.maxDep = depth
+
+        dft(root, 1)
+        return self.maxDep
